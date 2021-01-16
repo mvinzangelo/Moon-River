ea998e397edc02a999ff1b93264865437b662616
david-niculae <david.niculae@berkeley.edu>
2021-01-16 13:38:36 -0800

    obstacle script test

---
+++ New Unity Project/Assets/Scripts/test scripts/Rock.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyRock();
+    }
+
+    private void DestroyRock() 
+    {
+        Destroy(gameObject);
+    }
+}
