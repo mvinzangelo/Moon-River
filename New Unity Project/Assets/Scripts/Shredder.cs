2a8d2532a3ac8d9a109458dcdbcdb7e85c350726
MellowJelo <62518081+MellowJelo@users.noreply.github.com>
2021-01-16 13:02:43 -0800

    shredder created

---
+++ New Unity Project/Assets/Scripts/Shredder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour
+{
+    // destroy object when collided with
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // destroy the whole game object
+        Destroy(collision.gameObject);
+    }
+}
