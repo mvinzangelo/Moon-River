c750056a30a7a0af83ed7b71cf7f392dc1bf11e4
MellowJelo <62518081+MellowJelo@users.noreply.github.com>
2021-01-16 15:09:18 -0800

    score UI

---
+++ New Unity Project/Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreUI : MonoBehaviour
+{
+    [SerializeField] int currentScore = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            IncreaseScore(10);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            DecreaseScore(10);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ResetScore();
+        }
+
+    }
+
+    public void ResetScore()
+    {
+        currentScore = 0;
+        GetComponent<TextMeshProUGUI>().text = currentScore.ToString();
+    }
+    
+    public void IncreaseScore(int pointValue)
+    {
+        currentScore += pointValue;
+        GetComponent<TextMeshProUGUI>().text = currentScore.ToString();
+    }
+
+    public void DecreaseScore(int pointValue)
+    {
+        currentScore -= pointValue;
+        GetComponent<TextMeshProUGUI>().text = currentScore.ToString();
+    }
+}
