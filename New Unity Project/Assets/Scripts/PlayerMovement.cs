68c3e5ef5b89cf0443f12a9343edda6dcdf8e1dd
jecndlria <73074625+jecndlria@users.noreply.github.com>
2021-01-16 13:33:11 -0800

    josh - first player movement

--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float moveSpeed;
+
+    private Rigidbody2D rb;
+    private Rigidbody2D rb2;
+    private float moveDirectionHorizontal;
+    private float moveDirectionVertical;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb2 = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Get inputs
+        moveDirectionHorizontal = Input.GetAxis("Horizontal");
+        moveDirectionVertical = Input.GetAxis("Vertical");
         
+
+        //Move
+        rb.velocity = new Vector2(moveDirectionHorizontal * moveSpeed, rb.velocity.y);
+        rb2.velocity = new Vector2(moveDirectionVertical * moveSpeed, rb.velocity.x);
     }
 }
