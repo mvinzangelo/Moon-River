95a1dc39bb6654e6a7f026d78645b87529ca1a70
david-niculae <david.niculae@berkeley.edu>
2021-01-16 14:33:55 -0800

    david obstacle script 2

---
+++ New Unity Project/Assets/Scripts/Rock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    public float speed;
+    public bool MoveLeft;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyRock();
+    }
+
+    private void DestroyRock()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+        }
+    }
+
+}
