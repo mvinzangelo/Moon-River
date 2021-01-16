425c59fc868c0f73de742ba9ebfe69094034e1d5
jecndlria <73074625+jecndlria@users.noreply.github.com>
2021-01-16 15:12:14 -0800

    josh - first health system

---
+++ New Unity Project/Assets/Scripts/Health.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health
+{
+    private int health;
+    private int healthMax;
+    HeartsUI HeartsUI;
+
+    public Health(int healthMax)
+    {
+        this.healthMax = healthMax;
+        health = healthMax;
+    }
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public void Damage(int damageAmount)
+    {
+        health -= damageAmount;
+        HeartsUI.DecreaseHealth();
+        if (health < 0)
+        {
+            health = 0;
+        }
+    }
+
+    public void Heal(int healAmount)
+    {
+        health += healAmount;
+        HeartsUI.IncreaseHealth();
+        if (health > healthMax)
+        {
+            health = healthMax;
+        }
+        
+    }
+}
