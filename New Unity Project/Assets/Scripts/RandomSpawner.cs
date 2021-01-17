d5355ed0b63e95e9c53f556c08daa50680386958
jecndlria <73074625+jecndlria@users.noreply.github.com>
2021-01-17 00:59:17 -0800

    random chance rock and resized sprites

--- New Unity Project/Assets/Scripts/RandomSpawner.cs
+++ New Unity Project/Assets/Scripts/RandomSpawner.cs
@@ -8,6 +8,7 @@ public class RandomSpawner : MonoBehaviour
     public Transform[] spawnPoints;
     public GameObject[] enemyPrefabs;
     [SerializeField] float spawnDelay = 3f;
+    [SerializeField] float rockChance;
     // Start is called before the first frame update
     public void StartSpawning()
     {
@@ -15,11 +16,29 @@ public class RandomSpawner : MonoBehaviour
     }
     IEnumerator SpawnRandomEnemy()
     {
-        int randEnemy = Random.Range(0, enemyPrefabs.Length);
+        
+        int randEnemy = Random.Range(0, 100);
         int randSpawnPoint = Random.Range(0, spawnPoints.Length);
         int randomSpawn = Random.Range(0, enemyPrefabs.Length);
-        Instantiate(enemyPrefabs[randomSpawn], spawnPoints[randSpawnPoint].position, transform.rotation);
+        int flipCoin = Random.Range(0, 1);
+        //Instantiate(enemyPrefabs[randomSpawn], spawnPoints[randSpawnPoint].position, transform.rotation);
+        if (randEnemy >= 1 && randEnemy <= rockChance)
+        {
+            Instantiate(enemyPrefabs[1], spawnPoints[randSpawnPoint].position, transform.rotation);
+        }
+        else 
+        {
+            if (flipCoin == 0)
+            {
+                Instantiate(enemyPrefabs[0], spawnPoints[randSpawnPoint].position, transform.rotation);
+            }
+            else
+            {
+                Instantiate(enemyPrefabs[2], spawnPoints[randSpawnPoint].position, transform.rotation);
+            }
+        }
         yield return new WaitForSeconds(spawnDelay);
         StartCoroutine(SpawnRandomEnemy());
+        
     }
 }
