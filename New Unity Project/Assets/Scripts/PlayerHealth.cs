614c71d26ae6a064ef5914875d6f9f72a178630e
jecndlria <73074625+jecndlria@users.noreply.github.com>
2021-01-16 17:44:25 -0800

    josh first working version of player health

--- New Unity Project/Assets/Scripts/PlayerHealth.cs
+++ New Unity Project/Assets/Scripts/PlayerHealth.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerHealth : MonoBehaviour
 {
+    public int currentHealth;
+    Rock rock;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Health health = new Health(3);
+        currentHealth = health.GetHealth();
+        Debug.Log("Health: " + currentHealth);
     }
 
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    { 
+        Debug.Log("COLLISION");
+
+        if (collision.gameObject.name == "Rock")
+        {
+            currentHealth -= 1;
+            Debug.Log("Health: " + currentHealth);
+        }
+
+        if (collision.gameObject.name == "Flower")
+        {
+            currentHealth += 1;
+            Debug.Log("Health: " + currentHealth); 
+        }
+    }
     // Update is called once per frame
     void Update()
     {
