d2a562b1da806054acd671ff98c5fe0304385127
david-niculae <david.niculae@berkeley.edu>
2021-01-16 20:54:02 -0800

    chime sounds and boat breaking sounds

--- New Unity Project/Assets/Scripts/Flower.cs
+++ New Unity Project/Assets/Scripts/Flower.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 
 public class Flower : MonoBehaviour
 {
+    [SerializeField] AudioClip[] chime;
     public float speed;
     public bool MoveLeft;
     [SerializeField] ScoreUI scoreUI;
-    [SerializeField] int pointValue = 10; 
+    [SerializeField] int pointValue = 10;
+    [SerializeField] float chimeVolume = 0.2f;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         DestroyFlower();
+        int randomChime = Random.Range(0, chime.Length);
+        AudioSource.PlayClipAtPoint(chime[randomChime], Camera.main.transform.position, chimeVolume);
     }
 
     private void DestroyFlower()
     {
+        
         scoreUI.IncreaseScore(pointValue);
         Destroy(gameObject);
     }
--- New Unity Project/Assets/Scripts/Rock.cs
+++ New Unity Project/Assets/Scripts/Rock.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class Rock : MonoBehaviour
 {
+    [SerializeField] AudioClip[] damage;
+    [SerializeField] float damageVolume = .2f;
+    [SerializeField] int pointValue = -20;
+    [SerializeField] ScoreUI scoreUI;
     public float speed;
     public bool MoveLeft;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         DestroyRock();
+        int randomDamage = Random.Range(0, damage.Length);
+        AudioSource.PlayClipAtPoint(damage[randomDamage], Camera.main.transform.position, damageVolume);
+
     }
 
     private void DestroyRock()
     {
+        scoreUI.DecreaseScore(pointValue);
         Destroy(gameObject);
     }
 
